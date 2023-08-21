879fd12a01e867ae6c972e794b49e491702f6a04
tuantotti <tuantotti14032001@gmail.com>
2023-08-21 09:56:46 +0700

    refactor code multilabel tf idf task

---
+++ multllabel/base_multilabel.py
@@ -0,0 +1,47 @@
+from typing import Any
+from skmultilearn.problem_transform import LabelPowerset, BinaryRelevance
+from sklearn.naive_bayes import GaussianNB
+from sklearn.multioutput import ClassifierChain
+from sklearn.linear_model import LogisticRegression
+from skmultilearn.adapt import MLkNN
+import numpy as np
+
+
+class MultilabelModel:
+    def __init__(self, X_train, y_train, X_test, method='LabelPowerset', num_classes=4) -> None:
+        self.method = method
+        self.num_classes = num_classes
+        self.X_train, self.y_train, self.X_test= X_train, y_train, X_test
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        y_pred = []
+        if self.method == 'LabelPowerset':
+            classifier = LabelPowerset(LogisticRegression())
+            classifier.fit(self.X_train, self.y_train)
+            y_pred = classifier.predict(self.X_test)
+
+        elif self.method == 'BinaryRelevance':
+            classifier = BinaryRelevance(GaussianNB())
+            classifier.fit(self.X_train, self.y_train)
+            y_pred = classifier.predict(self.X_test)
+
+        elif self.method == 'ClassifierChain':
+            base_lr = LogisticRegression()
+
+            chains = [ClassifierChain(base_lr, order="random", random_state=i) for i in range(num_classes)]
+            for chain in chains:
+                chain.fit(self.X_train, self.y_train)
+
+            Y_pred_chains = np.array([chain.predict(self.X_test) for chain in chains])
+
+            y_pred = Y_pred_chains.mean(axis=0)
+
+        elif self.method == 'MLkNN':
+            classifier = MLkNN(k=self.num_classes)
+            classifier.fit(self.X_train, self.y_train)
+            y_pred = classifier.predict(self.X_test)
+
+        else:
+            print('No method chosen')
+
+        return y_pred
--- multllabel/multilabel_tfidf.py
+++ multllabel/multilabel_tfidf.py
@@ -2,16 +2,11 @@ import pandas as pd
 import os
 import numpy as np
 
-from sklearn.metrics import classification_report, accuracy_score, jaccard_score
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from skmultilearn.problem_transform import LabelPowerset, BinaryRelevance
-from sklearn.naive_bayes import GaussianNB
-from sklearn.multioutput import ClassifierChain
-from sklearn.linear_model import LogisticRegression
-from skmultilearn.adapt import MLkNN
-from sklearn.pipeline import Pipeline
-from sklearn.multiclass import OneVsRestClassifier
+from base_multilabel import MultilabelModel
+from ..feature_extraction_utils import TfIdf
 
 """
 Read and preprocess data
@@ -23,10 +18,13 @@ data = data.drop(['Unnamed: 0', 'index', 'ADDRESS', 'LABEL_FORMAT'], axis=1)
 # remove_label = data['LABEL'].value_counts().keys().tolist()[6:]
 # remove_index = data[data['LABEL'].isin(remove_label)].index
 # data.drop(remove_index, inplace=True)
-X, y = data['BYTECODE'], data.iloc[:, -9:].to_numpy()
+num_classes = 4
+selected_columns = ['BYTECODE', 'Timestamp dependence', 'Outdated Solidity version', 'Frozen Ether', 'Delegatecall Injection']
+data = data.loc[:, selected_columns]
+X, y = data['BYTECODE'], data.iloc[:, -num_classes:].to_numpy()
 print('y: ', y.shape)
-labels = data.iloc[:, -9:].keys().tolist()
-values = np.sum(labels, axis=0)
+labels = data.iloc[:, -num_classes:].keys().tolist()
+values = np.sum(y, axis=0)
 
 print(dict(zip(labels, values)))
 
@@ -38,26 +36,13 @@ def save_classification(y_test,y_pred, out_dir):
 
   return out_df
 
+"""## Split data"""
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
 """## Feature Extraction
 ### TF IDF
 """
 print("Feature Extraction - TFIDF")
-class TfIdf:
-    def __init__(self, X_train, X_test):
-        self.X_train = X_train
-        self.X_test = X_test
-        self.vectorized = TfidfVectorizer()
-
-    def __call__(self, *args, **kwargs):
-        # create vocabulary and calculate idf
-        X_train = self.vectorized.fit_transform(raw_documents=self.X_train).toarray()
-
-        X_test = self.vectorized.transform(raw_documents=self.X_test).toarray()
-
-        return X_train, X_test
-
-"""## Split data"""
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 tfidf = TfIdf(X_train, X_test)
 X_train, X_test = tfidf()
 
@@ -66,41 +51,31 @@ X_train, X_test = tfidf()
 """
 print("Multilabel machine learning")
 print("Label Powerset")
-classifier = LabelPowerset(LogisticRegression())
-classifier.fit(X_train, Y_train)
-y_pred_lbp = classifier.predict(X_test)
+lbl_powerset = MultilabelModel(X_train=X_train, y_train=Y_train, X_test=X_test, 
+                               method='LabelPowerset', num_classes=num_classes)
+y_pred_lbp = lbl_powerset()
 
 save_classification(y_test=Y_test, y_pred=y_pred_lbp, out_dir='.././report/Label_Powerset_TFIDF.csv')
 
 """### Binary relevence"""
 print("Binary relevence")
 # with a gaussian naive bayes base classifier
-classifier = BinaryRelevance(GaussianNB())
-classifier.fit(X_train, Y_train)
-y_pred_binre = classifier.predict(X_test)
+bin_relevence = MultilabelModel(X_train=X_train, y_train=Y_train, X_test=X_test, 
+                               method='BinaryRelevance', num_classes=num_classes)
+y_pred_binre = bin_relevence()
 save_classification(y_test=Y_test, y_pred=y_pred_binre, out_dir='.././report/Binary_Relevence_TFIDF.csv')
 
 """### Classifier Chains"""
 print("Classifier Chains")
-base_lr = LogisticRegression()
-
-chains = [ClassifierChain(base_lr, order="random", random_state=i) for i in range(9)]
-for chain in chains:
-    chain.fit(X_train, Y_train)
-
-Y_pred_chains = np.array([chain.predict(X_test) for chain in chains])
-chain_jaccard_scores = [
-    jaccard_score(Y_test, Y_pred_chain >= 0.5, average="samples")
-    for Y_pred_chain in Y_pred_chains
-]
-
+classifier_chain = MultilabelModel(X_train=X_train, y_train=Y_train, X_test=X_test, 
+                               method='ClassifierChain', num_classes=num_classes)
+Y_pred_chains = classifier_chain()
 Y_pred_ensemble = Y_pred_chains.mean(axis=0)
-ensemble_jaccard_score = jaccard_score(Y_test, Y_pred_ensemble >= 0.5, average="samples")
-save_classification(y_test=Y_test, y_pred=Y_pred_ensemble, out_dir='.././report/Classifier_Chains_TFIDF.csv')
+save_classification(y_test=Y_test, y_pred=Y_pred_ensemble.astype(int), out_dir='.././report/Classifier_Chains_TFIDF.csv')
 
 """### Adapted Algorithm"""
 print("Adapted Algorithm")
-classifier = MLkNN(k=8)
-classifier.fit(X_train, Y_train)
-y_pred = classifier.predict(X_test)
-save_classification(y_test=Y_test, y_pred=Y_pred_ensemble, out_dir='.././report/Adapted_Algorithm_TFIDF.csv')
+adapt_al = MultilabelModel(X_train=X_train, y_train=Y_train, X_test=X_test, 
+                               method='MLkNN', num_classes=num_classes)
+y_pred_adapt = adapt_al()
+save_classification(y_test=Y_test, y_pred=y_pred_adapt.astype(int), out_dir='.././report/Adapted_Algorithm_TFIDF.csv')
